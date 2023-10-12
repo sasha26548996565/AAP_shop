295c8be84db535fc3e27f978656eb90fbe2514f3
Sasha <sashapozhidaev07@gmail.com>
2023-10-13 00:13:00 +0600

    check response message telegram

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -10,11 +10,24 @@ final class TelegramBotApi
 {
     public const HOST = 'https://api.telegram.org/bot';
 
-    public static function sendMessage(string $token, int $chatId, string $text)
+    public static function sendMessage(string $token, int $chatId, string $text): bool
     {
-        Http::get(self::HOST . $token . '/sendMessage', [
-            'chat_id' => $chatId,
-            'text' => $text
-        ]);
+        try
+        {
+            $response = Http::get(self::HOST . $token . '/sendMessage', [
+                'chat_id' => $chatId,
+                'text' => $text
+            ]);
+            
+            if ($response->successful())
+            {
+                return true;
+            }
+    
+            return false;
+        } catch (\Exception $exception)
+        {
+            throw $exception->getMessage();
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    logger()
+        ->channel('telegram')
+        ->debug('some message//.');
     return view('welcome');
 });
